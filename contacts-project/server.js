5510116cb3e9c9adcf3d6051a7dde86fe3fcb386
Dmytro <dima4994@gmail.com>
2024-06-15 18:33:24 +0300

    Added the Get and GetAll endpoints

--- contacts-project/server.js
+++ contacts-project/server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect.js');
+const contactsRoutes = require('./routes/contacts.js');
 const app = express();
+
 
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  });
+ app.use('/contacts', contactsRoutes);
+
+ app.get('/', (req, res) => {
   res.send('Hello World!!');
 })
 
-
-app.listen(process.env.port || port);
-console.log('Web Server is litsening t port ' + (process.env.port || port));
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
